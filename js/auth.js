5cdefa9fa5afaf21f4e3405f46a8862bee93eae6
yelasri <elasriyoussef604@gmail.com>
2025-05-17 17:28:26 +0100

    update main js

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,21 @@
 export function loginPage() {
     document.body.innerHTML = /*html*/`
-        <form>
+        <form class="loginForm">
             <h2>Login</h2>
             <label for="login">Email or Username</label>
-            <input type="text" id="login">
+            <input name="login" type="text" id="login">
             <label for="password">Password</label>
-            <input type="password" id="password">
+            <input name="password" type="password" id="password">
             <button type="submit">Login</button>
         </form>
     `
 
-    
+    let formElement = document.querySelector('.loginForm')
+    formElement.addEventListener('submit', e => {
+        e.preventDefault()
+
+        const formData = Object.fromEntries(new FormData(formElement).entries());
+
+        console.log(formData);
+    })
 }
--- main.js
+++ main.js
@@ -1,32 +1,6 @@
 import { loginPage } from "./js/auth.js"
 
-export function navigateTo(url) {
-    history.pushState(null, null, url)
-    router()
-}
-
-function router() {
-    let routes = [
-        { path: "/", view: "" },
-        { path: "/login", view: loginPage }
-    ]
-
-    let potentialMatches = routes.map(route => {
-        return {
-            route: route,
-            isMatch: location.pathname === route.path
-        }
-    })
-
-    let match = potentialMatches.find(p => p.isMatch)
-    if (!match) {
-        console.log('Page not found')
-        return
-    }
-
-    match.route.view()
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-    navigateTo('/login')
+   history.pushState(null, null, "/")
+   loginPage()
 })
