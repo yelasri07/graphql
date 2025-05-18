6f3f0cd9cf1c58ecec8bd0895f98dcdac2b22465
yelasri <elasriyoussef604@gmail.com>
2025-05-18 16:47:30 +0100

    style home page

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { loginPage } from "./js/auth.js"
+import { homePage } from "./js/homePage.js"
 
 document.addEventListener('DOMContentLoaded', () => {
    history.pushState(null, null, "/")
-   loginPage()
+   homePage()
 })
