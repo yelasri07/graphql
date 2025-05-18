c44833238331363de6e9400a31deffb28326f7a9
yelasri <elasriyoussef604@gmail.com>
2025-05-18 15:01:55 +0100

    graph

--- js/homePage.js
+++ js/homePage.js
@@ -1,3 +1,16 @@
+function header() {
+    return /*html*/`
+        <header>
+            <nav>
+                <a href="/" class="logo">GRAPHQL</a>
+                <div>
+                    
+                </div>
+            </nav>
+        </header>
+    `
+}
+
 export function homePage() {
     document.body.innerHTML = /*html*/`
         <h1>Hello Youssef</h1>
